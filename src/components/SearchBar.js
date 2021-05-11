7ee3d4450d99b4467ed22f7dfb05ffa669de5b62
Susana Duque <su.duquem@gmail.com>
2021-05-10 23:45:09 -0500

    Se refactoriza el componente SearchBar haciendo uso del hook useState

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+// Este componente solo tiene state, por tanto solo se requiere el useState hook
 
-class SearchBar extends React.Component {
-  // Se convertirá el input de incontrolado a controlado,
-  // con el fin de almacenar el término de búsqueda en el componente y no en el DOM
+const SearchBar = ({ onFormSubmitApp }) => {
+  const [term, setTerm] = useState('');
 
-  state = { term: '' };
-
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
-
-  onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
 
     // calling the callback from parent component
-    // Se le pasa al componente padre el término de búsqueda, para que lo conozca
-    this.props.onFormSubmitApp(this.state.term);
+    /* Se le pasa al componente padre el término de búsqueda, para que lo conozca */
+    onFormSubmitApp(term);
   };
 
-  render() {
-    return (
-      <div className='search-bar ui segment'>
-        <form onSubmit={this.onFormSubmit} className='ui form'>
-          <div className='field'>
-            <label>Video Search</label>
-            <input
-              type='text'
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='search-bar ui segment'>
+      <form onSubmit={onSubmit} className='ui form'>
+        <div className='field'>
+          <label>Video Search</label>
+          <input
+            type='text'
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
