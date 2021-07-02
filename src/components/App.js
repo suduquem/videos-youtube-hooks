a81102bcdf486c213062c67b8e1e4bfb35dd02b9
Susana Duque <su.duquem@gmail.com>
2021-07-02 18:53:15 -0500

    Se refactoriza el componente App haciendo uso de useState y useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
 import VideoDetail from './videoDetail';
 import VideoList from './VideoList';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+/* Este componente hace uso de un state que declara 2 propiedades,
+por tanto el hook useState se usará 2 veces. Además usa el lifecycle
+method componentDidMount, lo que significa que se debe usar el hook
+useEffect. El componentDidMount solo se ejecuta 1 vez, cuando se
+renderiza por primera vez el componente App en la pantalla, por eso
+el segundo argumento de useEffect será un array vacío que cumpla con
+la equivalencia */
 
-  componentDidMount() {
-    this.onTermSubmit('cute cats');
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onTermSubmit('cute cats');
     // Para que cuando cargue la app se muestre una búsqueda por defecto
-  }
+  }, []); //Empty array: run the function only one time
 
   // Callback que se llamará cuando se vaya a realizar una búsqueda
   // El término (term) se pasa desde el componente hijo SearchBar
   // Asynchronous API request
   // Se debe usar una promesa o la sintáxis de Async Await
-  // onTermSubmit = (term) => {
-  //   console.log('término de SearchBar en App', term);
-  //   youtube.get('/search', { params: { q: term } });
-  // };
-  onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', { params: { q: term } });
-    console.log(response.data.items);
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
+  const onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
     });
-    // Seteamos selectedVideo también,
-    // para que cuando se realice una búsqueda, se muestre un video por defecto
+
+    console.log(response.data.items);
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+    /* Seteamos selectedVideo también, para que cuando se realice una
+    búsqueda, se muestre un video por defecto */
   };
 
-  onVideoSelect = (video) => {
+  const onVideoSelect = (video) => {
     console.log('From the App!', video);
-    this.setState({ selectedVideo: video });
+    setSelectedVideo(video);
     // Como se actualiza el State, se vuelve a renderizar el componente
   };
 
-  render() {
-    return (
-      <div className='ui container'>
-        <SearchBar onFormSubmitApp={this.onTermSubmit} />
-        {/* <p>I have {this.state.videos.length} videos</p> */}
-        <div className='ui grid'>
-          <div className='ui row'>
-            <div className='eleven wide column'>
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className='five wide column'>
-              <VideoList
-                videos={this.state.videos}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  return (
+    <div className='ui container'>
+      <SearchBar onFormSubmitApp={onTermSubmit} />
+      {/* <p>I have {videos.length} videos</p> */}
+      <div className='ui grid'>
+        <div className='ui row'>
+          <div className='eleven wide column'>
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className='five wide column'>
+            <VideoList
+              videos={videos}
+              onVideoSelect={onVideoSelect}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
